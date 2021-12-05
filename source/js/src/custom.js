1b74347f5f80eec09a2d898ac33179f479285fea
7mA <guodjcn@yahoo.co.jp>
2021-12-06 01:14:18 +0900

    <add>custom.js

--- source/js/src/custom.js
+++ source/js/src/custom.js
@@ -1,1 +1,11 @@
+let posts = document.getElementsByClassName('post-content');
 
+for(let post of posts) {
+  post.innerHTML = markdownRubyExtension(post.innerHTML);
+}
+
+function markdownRubyExtension(str) {
+    return str.replace(/\{([^{}()]+)\}\(([^{}()]+)\)/g, function(match, $1, $2) {
+        return '<ruby>' + $1 + '<rt>' + $2 + '</rt></ruby>'
+    });
+}
